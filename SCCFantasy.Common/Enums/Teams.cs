28cc999b636afcef63a021f6c162aae97d593b51
minhkhuele43 <minhkhuele43@gmail.com>
2023-07-15 16:18:19 +0700

    Players Info

---
+++ SCCFantasy.Common/Enums/Teams.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCCFantasy.Common.Enums
+{
+    public enum Teams
+    {
+        [Description("Arsenal")]
+        ARS = 1,
+
+        [Description("Aston Villa")]
+        AVL,
+
+        [Description("Bournemouth")]
+        BOU,
+
+        [Description("Brentford")]
+        BRE,
+
+        [Description("Brighton")]
+        BHA,
+
+        [Description("Burnley")]
+        BUR,
+
+        [Description("Chelsea")]
+        CHE,
+
+        [Description("Crystal Palace")]
+        CRY,
+
+        [Description("Everton")]
+        EVE,
+
+        [Description("Fulham")]
+        FUL,
+
+        [Description("Liverpool")]
+        LIV,
+
+        [Description("Luton")]
+        LUT,
+
+        [Description("Man City")]
+        MCI,
+
+        [Description("Man Utd")]
+        MUN,
+
+        [Description("Newcastle")]
+        NEW,
+
+        [Description("Nott'm Forest")]
+        NFO,
+
+        [Description("Sheffield Utd")]
+        SHU,
+
+        [Description("Spurs")]
+        TOT,
+
+        [Description("West Ham")]
+        WHU,
+
+        [Description("Wolves")]
+        WOL
+    }
+}
