1f853e9755748b832b2b834001ff9abd65a89e39
minhkhuele43 <minhkhuele43@gmail.com>
2023-07-18 00:16:03 +0700

    Remove hard code for next 5 fixtures

---
+++ SCCFantasy.Common/Enums/Difficulties.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCCFantasy.Common.Enums
+{
+    public enum Difficulties
+    {
+        Easy = 2,
+
+        Normal,
+
+        Hard,
+
+        VeryHard
+    }
+}
