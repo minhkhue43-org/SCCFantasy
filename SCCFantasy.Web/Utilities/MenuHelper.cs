7f09ff9e6b0ff6bc2c396fa27f53375e338c5600
minhkhuele43 <minhkhuele43@gmail.com>
2023-07-29 09:25:25 +0700

    Active Menu Link

---
+++ SCCFantasy.Web/Utilities/MenuHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SCCFantasy.Web.Utilities
+{
+    public static class MenuHelper
+    {
+        public static string IsSelected(this IHtmlHelper html, string controller = null, string action = null, string cssClass = null)
+        {
+            if (string.IsNullOrEmpty(cssClass))
+            {
+                cssClass = "active";
+            }
+
+            string currentAction = (string)html.ViewContext.RouteData.Values["action"];
+            string currentController = (string)html.ViewContext.RouteData.Values["controller"];
+
+            if (string.IsNullOrEmpty(controller))
+            {
+                controller = currentController;
+            }
+
+            if (string.IsNullOrEmpty(action))
+            {
+                action = currentAction;
+            }
+
+            return controller == currentController && action == currentAction ? cssClass : string.Empty;
+        }
+    }
+}
